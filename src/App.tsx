dbe85ad8bb2323b46f9983277e7e0a62cdd9925b
Choi Yong Won <cywin0821@gmail.com>
2025-02-24 12:40:43 +0900

    fix : App.tsx에서 GlobalStyles가 App 내부에 선언되어 있어서 경고 뜨는것을 외부로 옮김

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  html, body {
+    width: 100%;
+    height: 100%;
+    max-width: 1440px;
+    max-height: 1024px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+`;
+
 function App() {
-    const GlobalStyles = createGlobalStyle`
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        html, body {
-            width : 100%;
-            height : 100%;
-            max-width: 1440px;
-            max-height: 1024px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: black;
-            &::-webkit-scrollbar {
-                display: none;
-            }
-        }
-    `;
-
     return (
         <>
             <GlobalStyles />
