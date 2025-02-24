4553ee0feaa0df144b25be2efde50cbb2c67392a
Choi Yong Won <cywin0821@gmail.com>
2025-02-24 12:42:03 +0900

    feat : router.tsx에 upload 로직 추가 및 UploadPage.tsx 추가생성 (로그인 로직 후, url 수정예정)

---
+++ src/pages/upload/UploadPage.tsx
@@ -0,0 +1,4 @@
+const UploadPage = () =>{
+    return <div>hello</div>
+}
+export default UploadPage
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import UploadPage from "./pages/upload/UploadPage.tsx";
 const router = createBrowserRouter([
-
+    {
+        path: "/upload",
+        element: <UploadPage />,
+    },
 ]);
 
 export default router;
