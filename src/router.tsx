8fa5a109d2a70ec27c23fa14d6e82f34f0901adf
psm1st <psm1st@sookmyung.ac.kr>
2025-02-25 23:14:37 +0900

    feat : layout 제작 완료

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import UploadPage from "./pages/upload/UploadPage.tsx";
+import Layout from "./component/layout";
+import HomePage from "./page/home";
 const router = createBrowserRouter([
     {
         path: "/upload",
         element: <UploadPage />,
     },
+    {
+        path: "/home",
+        element: <Layout />,
+        children: [{ path: "", element: <HomePage /> }]
+      },
 ]);
 
 export default router;
