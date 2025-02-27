1631362c471b825f441d628a18a09639cba5687a
psm1st <psm1st@sookmyung.ac.kr>
2025-02-27 23:50:23 +0900

    feat : 로고에 /home 로직 추가

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import UploadPage from "./pages/upload/UploadPage.tsx";
 import Layout from "./component/layout";
 import HomePage from "./page/home";
+import SearchPage from "./page/searchPage";
 const router = createBrowserRouter([
     {
         path: "/upload",
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
         path: "/home",
         element: <Layout />,
         children: [{ path: "", element: <HomePage /> }]
+      },
+      {
+        path: "/search",
+        element: <Layout />,
+        children: [{ path: "", element: <SearchPage /> }]
       },
 ]);
 
