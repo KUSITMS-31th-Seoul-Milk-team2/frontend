7315e7cdec3fdeb8c45f8be540908f4f3bed33b4
psm1st <psm1st@sookmyung.ac.kr>
2025-03-02 23:44:22 +0900

    fix : 예전에 세팅해둔 global style 폐기

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import { createGlobalStyle,ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import {theme} from "@styles//theme.ts";
 import router from "./router";
 
 
-const GlobalStyles = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  
-  html, body, #root {
-    width: 100%;
-    height: 100%;
-    max-width: 1440px;
-      min-width: 480px;
-    max-height: 1024px;
-      min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
-`;
-
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <GlobalStyles />
             <RouterProvider router={router} />
         </ThemeProvider>
     );
