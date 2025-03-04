87c6367269168bdc401fdfcfe2ca69be2a36999b
Choi Yong Won <cywin0821@gmail.com>
2025-03-04 15:12:07 +0900

    feat : 바로가기 뷰 작성

---
+++ src/components/home/Card.tsx
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+import ArrowRight from "@assets/icons/arrowRightIcon.svg";
+
+
+interface CardProps{
+    title: string;
+    bgColor?: string;
+    buttonText?: string;
+    imageSrc?: string;
+    onButtonClick?: () => void;
+    color?:string;
+}
+
+const Card = ({title,bgColor,buttonText,imageSrc, onButtonClick,color}:CardProps)=>{
+    return ( <CardContainer $bgColor={bgColor}>
+        <Title $color={color}>{title}</Title>
+        <CardContent>
+            <CardImage src={imageSrc} alt={title} />
+            <CardButton $color={color} onClick={onButtonClick}>{buttonText} <Arrow>
+                <img
+                src={ArrowRight}
+                alt="ArrowRight Icon"
+                style={{width: "1.5rem", height: "auto"}}
+            /></Arrow></CardButton>
+        </CardContent>
+    </CardContainer>)
+}
+export default Card
+
+
+const CardContainer = styled.div<{ $bgColor?: string }>`
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    flex-direction: column;
+    margin:1rem;
+    width: 30rem;
+    height: 14.5rem;
+    border-radius: 8px;
+    background-color: ${({ $bgColor }) => $bgColor};
+`;
+const CardContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    gap: 1rem;
+`;
+const Title = styled.h2<{ $color?: string }>`
+    height: 3rem;
+    margin-left: 2rem;
+    color: ${({ $color }) => $color};
+    font-size: ${({ theme }) => theme.typography.headlineM.fontSize};
+    font-weight: ${({ theme }) => theme.typography.headlineM.fontWeight};
+    line-height: ${({ theme }) => theme.typography.headlineL.lineHeight};
+`;
+
+const CardImage = styled.img`
+    width: 100%;
+    max-width: 180px;
+    height: auto;
+    align-self: flex-start;
+`;
+
+
+const CardButton = styled.button<{ $color?: string,$bgColor?: string }>`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0.8rem 4.5rem;
+    white-space: nowrap;
+    align-self: flex-end;
+    background-color: ${({ $color }) => $color};
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.typography.buttonM.fontSize};
+    font-weight: ${({ theme }) => theme.typography.buttonM.fontWeight};
+    border: none;
+    border-radius: 12px 12px 12px 4px;
+    cursor: pointer;
+    box-shadow: ${({ theme }) => theme.shadows.shadow100};
+    &:hover {
+        background-color: ${({ $bgColor }) => $bgColor};
+    }
+`;
+
+const Arrow = styled.span`
+    
+`;
