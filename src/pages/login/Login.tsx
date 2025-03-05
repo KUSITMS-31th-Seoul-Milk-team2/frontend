623216de8520fb3c2e1423d467e3f877ac85dce3
psm1st <psm1st@sookmyung.ac.kr>
2025-03-05 13:20:13 +0900

    feat : 모달창 등록

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -36,6 +36,7 @@ const Login = () => {
   
         const data = await response.json();
         console.log("로그인 성공:", data);
+        localStorage.setItem("userInfo", JSON.stringify(data.data.userInfo));
   
         navigate("/");
       } catch (err: any) {
