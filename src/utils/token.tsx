62d1d9d6ba3e84d71329b30c9831c991b8cb9ef3
psm1st <psm1st@sookmyung.ac.kr>
2025-03-11 01:43:05 +0900

    fix : 토큰 저장 로직 수정

--- src/utils/token.tsx
+++ src/utils/token.tsx
@@ -1,57 +1,57 @@
-import axios from "axios";
-
-const getCookie = (name: string): string | null => {
-  const cookies = document.cookie.split("; ");
-  for (let cookie of cookies) {
-    const [key, value] = cookie.split("=");
-    if (key === name) {
-      return decodeURIComponent(value);
-    }
-  }
-  return null;
-};
-
-console.log("원래 쿠키 : ",document.cookie);
-const token = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-  withCredentials: true,
-  headers: {
-    "ngrok-skip-browser-warning": "true",
-  },
-});
-
-token.interceptors.request.use(
-  (config) => {
-    const token = getCookie("accessToken");
-    console.log("토큰 : ",token);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-export default token;
 // import axios from "axios";
+
+// const getCookie = (name: string): string | null => {
+//   const cookies = document.cookie.split("; ");
+//   for (let cookie of cookies) {
+//     const [key, value] = cookie.split("=");
+//     if (key === name) {
+//       return decodeURIComponent(value);
+//     }
+//   }
+//   return null;
+// };
+
+// console.log("원래 쿠키 : ",document.cookie);
 // const token = axios.create({
 //   baseURL: import.meta.env.VITE_BACKEND_URL,
+//   withCredentials: true,
 //   headers: {
 //     "ngrok-skip-browser-warning": "true",
 //   },
 // });
-// token.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   console.log("Access Token:", token);
-//   console.log("Authorization Header:", `Bearer ${token}`);
 
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
+// token.interceptors.request.use(
+//   (config) => {
+//     const token = getCookie("accessToken");
+//     console.log("토큰 : ",token);
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
 //   }
-//   return config;
-// });
-
+// );
 
 // export default token;
+import axios from "axios";
+const token = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
+});
+token.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  console.log("Access Token:", token);
+  console.log("Authorization Header:", `Bearer ${token}`);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
+export default token;
