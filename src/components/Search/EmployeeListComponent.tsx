390d0aa13a8cd549f2b52caafe56a619142a491b
psm1st <psm1st@sookmyung.ac.kr>
2025-03-11 01:24:53 +0900

    fix : admin 지급결의서 조회

--- src/components/Search/EmployeeListComponent.tsx
+++ src/components/Search/EmployeeListComponent.tsx
@@ -121,7 +121,7 @@ export default EmployeeListComponent;
 const Container = styled.div`
   width: 100%;
   max-width: 1040px;
-  margin-top: 30px;
+  margin-top: 10px;
 `;
 
 const ExcelButton = styled.button`
