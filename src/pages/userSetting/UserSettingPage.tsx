b242b330dd7e33543b5d45a093080c128cca1bb0
psm1st <psm1st@sookmyung.ac.kr>
2025-03-12 15:01:45 +0900

    feat : 추가 API 설정 완

--- src/pages/userSetting/UserSettingPage.tsx
+++ src/pages/userSetting/UserSettingPage.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import UserSearch from "@components/userSetting/userSearch";
 import UserList from "@components/userSetting/userList";
 
 const UserSettingPage: React.FC = () => {
   return (
     <PageContainer>
       <Title>사용자 권한 관리</Title>
-      <UserSearch />
       <UserList/>
     </PageContainer>
   );
